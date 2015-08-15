9101c83679bba73ba7a50fee28d648396d6e53f7
scheah <sebcheah@gmail.com>
2015-08-14 23:34:10 -0700

    Add Registration Page - not fully functioning yet
    Need to call DB API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+var Member = require('../models/member');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Testing!' });
+	res.render('home', { title: 'Testing!' });
+});
+
+router.get('/register', function(req, res) {
+    res.render('register', { });
+});
+
+router.post('/register', function(req, res) {
+	/*http.request(req, function(response) {
+		if(response.status() == 200) {
+			res.status(200).end();
+		}
+		else {
+			res.status(response.status()).end();
+		}
+	}).end();*/
+	res.redirect('/');
 });
 
 module.exports = router;
