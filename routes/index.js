92edc97608a2c779d670a6d7873273ecdc4d0ddd
scheah <sebcheah@gmail.com>
2015-08-15 17:09:09 -0700

    Added login/register pages + registration functionality

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var http = require('http');
+var request = require('request');
 var Member = require('../models/member');
 
 /* GET home page. */
@@ -8,20 +8,33 @@ router.get('/', function(req, res, next) {
 	res.render('home', { title: 'Testing!' });
 });
 
+router.get('/login', function(req, res) {
+    res.render('login', { });
+});
+
 router.get('/register', function(req, res) {
     res.render('register', { });
 });
 
 router.post('/register', function(req, res) {
-	/*http.request(req, function(response) {
-		if(response.status() == 200) {
-			res.status(200).end();
+	var formData = {
+		email: 				req.body.emailAddr,
+		hashedPassword:		req.body.pass,
+		firstName:			req.body.firstName,
+		lastName:			req.body.lastName
+	};
+	request.post({url:'http://localhost:3000/api/members', form: formData}, callback)
+	function callback (error, httpResponse, body) { 
+		if (error) {
+			res.render('error', {message: err.message, error: error});
 		}
-		else {
-			res.status(response.status()).end();
+		else if (httpResponse.statusCode == 200) {
+			res.redirect('/');
 		}
-	}).end();*/
-	res.redirect('/');
+		else { //TODO expand else statements for specific error codes, and change user error handling
+			res.render('register', {message: 'An error occured: ' + httpResponse.statusCode});
+		}
+	}
 });
 
 module.exports = router;
